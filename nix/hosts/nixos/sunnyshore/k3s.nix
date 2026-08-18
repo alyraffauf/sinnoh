80d3ea9108898c908ecc1a05c2a3edde5f91f095
Aly Raffauf <aly@aly.codes>
2026-08-18 19:24:29 -0400

    feat: back up local-path service data

--- nix/hosts/nixos/sunnyshore/k3s.nix
+++ nix/hosts/nixos/sunnyshore/k3s.nix
@@ -32,5 +32,27 @@ _: {
         Persistent = true;
       };
     };
+
+    services.restic.backups.k3s-local-path = {
+      paths = ["/var/lib/rancher/k3s/storage"];
+      repository = "rclone:b2:aly-backups/sinnoh/sunnyshore/k3s-local-path";
+      extraBackupArgs = [
+        "--cleanup-cache"
+        "--exclude=/var/lib/rancher/k3s/storage/*_cnpg-system_pg-shared-1/**"
+      ];
+      initialize = true;
+      passwordFile = config.sops.secrets.restic-password.path;
+      pruneOpts = [
+        "--keep-daily 7"
+        "--keep-weekly 4"
+        "--keep-monthly 12"
+      ];
+      rcloneConfigFile = config.sops.secrets.rclone-b2.path;
+      timerConfig = {
+        OnCalendar = "daily";
+        Persistent = true;
+        RandomizedDelaySec = "3h";
+      };
+    };
   };
 }
