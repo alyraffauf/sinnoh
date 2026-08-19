852e73346173c1bbcc7a91136462cd8a8f95ccd7
Aly Raffauf <aly@aly.codes>
2026-08-19 11:58:05 -0400

    fix(terraform): route aly.town through sinnoh

--- terraform/aly_town.tf
+++ terraform/aly_town.tf
@@ -2,8 +2,7 @@ resource "cloudflare_dns_record" "aly_town_apex_a" {
   zone_id  = local.zones.aly_town
   name     = "aly.town"
   type     = "A"
-  # The apex remains on Johto; Vaultwarden below is served from Sinnoh.
-  content  = local.olivine
+  content  = local.sunnyshore
   proxied  = true
   ttl      = 1
   tags     = []
