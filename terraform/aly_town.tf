093874c9030e6d549109fe1333f87b39e8b9cfd8
Aly Raffauf <aly@aly.codes>
2026-08-18 22:24:09 -0400

    feat(terraform): manage aly.town DNS

---
+++ terraform/aly_town.tf
@@ -0,0 +1,12 @@
+# This name will move to Sinnoh with Pocket ID.  Keep the existing Olivine
+# target until that application migration is ready.
+resource "cloudflare_dns_record" "aly_town_apex_a" {
+  zone_id  = local.zones.aly_town
+  name     = "aly.town"
+  type     = "A"
+  content  = "51.81.32.154"
+  proxied  = true
+  ttl      = 1
+  tags     = []
+  settings = {}
+}
--- terraform/locals.tf
+++ terraform/locals.tf
@@ -4,6 +4,7 @@ locals {
   zones = {
     aly_codes    = "a6ac528f39180d8f53576a187feeba7f"
     aly_social   = "459ec51183a74a521f7003d46fbf2922"
+    aly_town     = "59b97b8a84552c8d80642504eed06de5"
     morsels_blue = "d983d1ec759d3a8cfee30d1c8a6e8603"
   }
 }
