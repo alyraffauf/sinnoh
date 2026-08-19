1b1feed09dc10db1e5032b8ff4f4d806b3086f89
Aly Raffauf <aly@aly.codes>
2026-08-18 22:25:29 -0400

    feat(identity): serve Pocket ID at aly.town

--- terraform/aly_town.tf
+++ terraform/aly_town.tf
@@ -10,3 +10,14 @@ resource "cloudflare_dns_record" "aly_town_apex_a" {
   tags     = []
   settings = {}
 }
+
+resource "cloudflare_dns_record" "aly_town_id_a" {
+  zone_id  = local.zones.aly_town
+  name     = "id.aly.town"
+  type     = "A"
+  content  = local.sunnyshore
+  proxied  = true
+  ttl      = 1
+  tags     = []
+  settings = {}
+}
