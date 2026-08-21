3c94b3b81b424ba88c4444385558d758e28f7b78
Aly Raffauf <aly@aly.codes>
2026-08-21 08:27:50 -0400

    deployments: put blzrd nodes in host configs

--- nix/hosts/nixos/canalave/default.nix
+++ nix/hosts/nixos/canalave/default.nix
@@ -3,25 +3,33 @@
   self,
   ...
 }: {
-  config.flake.nixosConfigurations.canalave = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+  config = {
+    flake.nixosConfigurations.canalave = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
-    modules = [
-      inputs.determinate.nixosModules.default
-      inputs.disko.nixosModules.disko
-      inputs.sops-nix.nixosModules.sops
-      self.nixosModules.autoUpgrade
-      self.nixosModules.default
-      self.nixosModules.canalave
-      self.nixosModules.aly
-      self.nixosModules.alloy
-      self.nixosModules.backups
-      self.nixosModules.k3s
-      self.nixosModules.prometheusNode
-      self.nixosModules.tailscale
-      self.nixosModules.wireguardSinnoh
-    ];
+      modules = [
+        inputs.determinate.nixosModules.default
+        inputs.disko.nixosModules.disko
+        inputs.sops-nix.nixosModules.sops
+        self.nixosModules.autoUpgrade
+        self.nixosModules.default
+        self.nixosModules.canalave
+        self.nixosModules.aly
+        self.nixosModules.alloy
+        self.nixosModules.backups
+        self.nixosModules.k3s
+        self.nixosModules.prometheusNode
+        self.nixosModules.tailscale
+        self.nixosModules.wireguardSinnoh
+      ];
 
-    specialArgs = {inherit self;};
+      specialArgs = {inherit self;};
+    };
+
+    blzrd.nodes.canalave = {
+      output = self.nixosConfigurations.canalave.config.system.build.toplevel;
+      type = "nixos";
+      user = "root";
+    };
   };
 }
--- nix/hosts/nixos/sunnyshore/default.nix
+++ nix/hosts/nixos/sunnyshore/default.nix
@@ -3,25 +3,33 @@
   self,
   ...
 }: {
-  config.flake.nixosConfigurations.sunnyshore = inputs.nixpkgs.lib.nixosSystem {
-    system = "x86_64-linux";
+  config = {
+    flake.nixosConfigurations.sunnyshore = inputs.nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
 
-    modules = [
-      inputs.determinate.nixosModules.default
-      inputs.disko.nixosModules.disko
-      inputs.sops-nix.nixosModules.sops
-      self.nixosModules.autoUpgrade
-      self.nixosModules.default
-      self.nixosModules.sunnyshore
-      self.nixosModules.aly
-      self.nixosModules.alloy
-      self.nixosModules.backups
-      self.nixosModules.k3s
-      self.nixosModules.prometheusNode
-      self.nixosModules.tailscale
-      self.nixosModules.wireguardSinnoh
-    ];
+      modules = [
+        inputs.determinate.nixosModules.default
+        inputs.disko.nixosModules.disko
+        inputs.sops-nix.nixosModules.sops
+        self.nixosModules.autoUpgrade
+        self.nixosModules.default
+        self.nixosModules.sunnyshore
+        self.nixosModules.aly
+        self.nixosModules.alloy
+        self.nixosModules.backups
+        self.nixosModules.k3s
+        self.nixosModules.prometheusNode
+        self.nixosModules.tailscale
+        self.nixosModules.wireguardSinnoh
+      ];
 
-    specialArgs = {inherit self;};
+      specialArgs = {inherit self;};
+    };
+
+    blzrd.nodes.sunnyshore = {
+      output = self.nixosConfigurations.sunnyshore.config.system.build.toplevel;
+      type = "nixos";
+      user = "root";
+    };
   };
 }
