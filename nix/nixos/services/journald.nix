c6ba6f9c04fbba4b12b516f864ffe2b10a3d6c32
Aly Raffauf <aly@aly.codes>
2026-09-07 11:16:10 -0400

    nix: simplify journald option path

--- nix/nixos/services/journald.nix
+++ nix/nixos/services/journald.nix
@@ -1,12 +1,10 @@
 _: {
   flake.nixosModules.default = {
-    services.journald = {
-      settings.Journal = {
-        Storage = "persistent";
-        SystemMaxUse = "1G";
-        SystemKeepFree = "1G";
-        MaxRetentionSec = "1week";
-      };
+    services.journald.settings.Journal = {
+      Storage = "persistent";
+      SystemMaxUse = "1G";
+      SystemKeepFree = "1G";
+      MaxRetentionSec = "1week";
     };
   };
 }
